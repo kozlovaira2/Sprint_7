a554a00bf73c6a6a21d2a25bca2caeb40e383e00
Irina Kozlova <irina.kozlova@vk.team>
2026-07-04 18:57:46 +0300

    requests moved from test to helper

--- tests/test_login_courier.py
+++ tests/test_login_courier.py
@@ -9,6 +9,7 @@ import pytest
 from utils.endpoints import Endpoints
 from utils.test_data import Users, CourierData
 from utils.error_messages import ErrorMessages
+from helpers import login_courier_request
 
 
 class TestLoginCourier:
@@ -16,12 +17,9 @@ class TestLoginCourier:
     @allure.title('Авторизация под курьером выдает id')
     def test_courier_log_in(self, create_courier):
         """Проверка успешной авторизации курьера"""
-        response = requests.post(
-            Endpoints.COURIER_LOGIN,
-            data={
-                "login": create_courier["login"],
-                "password": create_courier["password"]
-            }
+        response = login_courier_request(
+            create_courier["login"],
+            create_courier["password"]
         )
         assert response.status_code == 200
         assert 'id' in response.text
@@ -29,9 +27,9 @@ class TestLoginCourier:
     @allure.title('Ошибка при авторизации если логин или пароль не корректные')
     def test_courier_log_negative(self):
         """Проверка ошибки при неверном логине или пароле"""
-        response = requests.post(
-            Endpoints.COURIER_LOGIN,
-            data=Users.data_negative
+        response = login_courier_request(
+            Users.data_negative["login"],
+            Users.data_negative["password"]
         )
         assert response.status_code == 404
         assert ErrorMessages.AUTH_FAILED in response.text
@@ -39,25 +37,13 @@ class TestLoginCourier:
     @allure.title('Ошибка при авторизации если логин пустой')
     def test_courier_log_with_empty_login(self):
         """Проверка ошибки при авторизации с пустым логином"""
-        response = requests.post(
-            Endpoints.COURIER_LOGIN,
-            data={
-                "login": "",
-                "password": CourierData.DEFAULT_PASSWORD
-            }
-        )
+        response = login_courier_request("", CourierData.DEFAULT_PASSWORD)
         assert response.status_code == 400
         assert ErrorMessages.MISSING_AUTH_FIELDS in response.text
 
     @allure.title('Ошибка при авторизации если пароль пустой')
     def test_courier_log_with_empty_password(self):
         """Проверка ошибки при авторизации с пустым паролем"""
-        response = requests.post(
-            Endpoints.COURIER_LOGIN,
-            data={
-                "login": CourierData.DEFAULT_LOGIN,
-                "password": ""
-            }
-        )
+        response = login_courier_request(CourierData.DEFAULT_LOGIN, "")
         assert response.status_code == 400
         assert ErrorMessages.MISSING_AUTH_FIELDS in response.text
